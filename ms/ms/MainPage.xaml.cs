01849f2a7f2c66b4cbc6b465dd305f1737d0a1fe
Catherine <Catherine25@users.noreply.github.com>
2019-05-19 22:05:35 +0300

    Remove: - bookListViewPage; - bookPage; setBookPage() change;

--- ms/ms/MainPage.xaml.cs
+++ ms/ms/MainPage.xaml.cs
@@ -4,11 +4,8 @@ namespace ms
 {
     public partial class MainPage : ContentPage
     {
-        private UI.BookListViewPage bookListViewPage = new UI.BookListViewPage();
-        private UI.BookPage bookPage = new UI.BookPage();
-
         public void setBookListViewPage() => Navigation.PopToRootAsync();
-        public void setBookPage() => Navigation.PushAsync(bookPage);
+        public void setBookPage() => Navigation.PushAsync(new UI.BookPage());
 
         public MainPage() => InitializeComponent();
     }
