936178ebaa4447672e6317f54b1d0d153d2dfb89
Catherine <Catherine25@users.noreply.github.com>
2019-05-21 23:34:18 +0300

    Add INotifyPropertyChanged; Directly sets values;

--- ms/ms/UI/BookPage.xaml.cs
+++ ms/ms/UI/BookPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,24 @@ using Xamarin.Forms.Xaml;
 
 namespace ms.UI
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class BookPage : ContentPage
-	{
-        string Author, Desc;
-        ImageSource ImageSource;
-
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookPage : ContentPage, INotifyPropertyChanged
+    {
         public BookPage(Data.Book book)
         {
             InitializeComponent();
 
-            Author = book.Author;
-            Desc = book.Desc;
-            ImageSource = (FileImageSource)ImageSource.FromFile(book.Image);
+            //Author = book.Author;
+            //Desc = book.Desc;
+            //Image = (FileImageSource)ImageSource.FromFile(book.Image);
 
             System.Diagnostics.Debug.WriteLine("");
             System.Diagnostics.Debug.WriteLine("BookPage init");
             System.Diagnostics.Debug.WriteLine("");
-        }
 
-        private void Back_Clicked(object sender, EventArgs e) => Navigation.PopAsync();
+            this.authorLabel.Text = book.Author;
+            this.descLabel.Text = book.Desc;
+            this.imageName.Source = (FileImageSource)ImageSource.FromFile(book.Image);
+        }
     }
 }
